a58928f60f1e0b9ea7007d36a5b07fe9f99173a2
hieu <hmhieu18@apcs.vn>
2020-12-15 00:27:45 +0700

    ui

---
+++ app/src/main/java/com/example/lingophile/RegisterFromActivity.java
@@ -0,0 +1,14 @@
+package com.example.lingophile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterFromActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_from);
+    }
+}
