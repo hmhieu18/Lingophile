ff398c92b28c9f2ec95963da419172da98e0efe6
hieu <hmhieu18@apcs.vn>
2020-12-16 15:25:44 +0700

    Directory Design

--- app/src/main/java/com/example/lingophile/Activity/LoginActivity.java
+++ app/src/main/java/com/example/lingophile/Activity/LoginActivity.java
@@ -3,10 +3,11 @@ package com.example.lingophile.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.lingophile.Model.FirebaseManagement;
 import com.example.lingophile.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private FirebaseManagement firebaseManagement;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
